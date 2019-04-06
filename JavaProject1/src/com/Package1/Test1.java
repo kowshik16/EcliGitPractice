c0d0815e9c59859bcab640cb9dbf3668d5f41564
lenovo <kowshik16@gmail.com>
2019-04-06 19:40:51 +0530

    added new methods in the Test1.java file

--- JavaProject1/src/com/Package1/Test1.java
+++ JavaProject1/src/com/Package1/Test1.java
@@ -13,11 +13,16 @@ public class Test1 {
 		System.out.println("This is m2 in the Test1");
 	}
 	
+	public void m4()
+	{
+		// this is method 4 added in between m2 and m3
+	}
+	
 	public void m3()
 	{
 		// written by test2
 		System.out.println("This is m3 in the Test1 class file");
-		System.out.println("Done in the Test2 branch");
+		System.out.println("Done in the Test2 branch"); // updated by the Test2 user
 	}
 
 }
